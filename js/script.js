8848d1d192a9504971b8214db25831887bb554f1
Mgela19 <lashamgelashvili777@gmail.com>
2023-11-13 03:29:35 +0400

    CARENT website

---
+++ js/script.js
@@ -0,0 +1,119 @@
+// let navigationelement = document.getElementById ('navblock');
+// let burgerbar =document.getElementById ('burgerblock');
+
+// burgerbar.addEventListener ('click', function(){
+//     navigationelement.classList.toggle ("newnavigation");
+// });
+
+// $(document).ready(function(){
+//     $('.slider').slick({
+//         slidesToShow: 2,  // Change the number of slides shown
+//         autoplay: false, // Enable autoplay
+//         autoplaySpeed: 3000, // Autoplay speed in milliseconds (adjust as needed)
+//         arrows: true, // Show navigation arrows
+//         dots: true, // Show pagination dots
+//         infinite: false  // Set infinite to false
+//     });
+//   });
+  
+//   $(document).ready(function(){
+//       $('.slider').slick({
+//           slidesToShow: 2,
+//           autoplay: false,
+//           autoplaySpeed: 3000,
+//           arrows: true,
+//           dots: true,
+
+//       });
+  
+//       // Change the dot color to orange for both active and inactive dots
+//       $('.slider .slick-dots li button').css('color', 'orange');
+//   });
+  
+  
+
+
+  
+  
+  
+  
+//   // script.js
+//   window.addEventListener('load', function() {
+//     const welcomeAnimation = document.querySelector('.welcome-animation');
+//     welcomeAnimation.style.animation = 'none'; // Reset animation
+//     welcomeAnimation.offsetHeight; // Trigger a reflow
+//     welcomeAnimation.style.animation = null; // Re-enable animation
+  
+//     // Add an event listener to remove the element when the animation ends
+//     welcomeAnimation.addEventListener('animationend', function() {
+//       welcomeAnimation.remove();
+//     });
+//   });
+//   function scrollToTop() {
+//     window.scrollTo({ top: 0, behavior: 'smooth' }); // Smooth scroll to the top
+//   }
+//   window.addEventListener('load', scrollToTop);
+  
+
+
+
+let navigationelement = document.getElementById('navblock');
+let burgerbar = document.getElementById('burgerblock');
+
+burgerbar.addEventListener('click', function () {
+    navigationelement.classList.toggle("newnavigation");
+});
+
+$(document).ready(function () {
+    // Initialize Slick slider
+    $('.slider').slick({
+        slidesToShow: 2,
+        autoplay: false,
+        autoplaySpeed: 3000,
+        arrows: true,
+        dots: true,
+        responsive: [
+            {
+                breakpoint: 768, // Adjust this breakpoint as needed
+                settings: {
+                    slidesToShow: 1,
+                },
+            },
+            {
+                breakpoint: 992, // Adjust this breakpoint as needed
+                settings: {
+                    slidesToShow: 2,
+                },
+            },
+            {
+                breakpoint: 1200, // Adjust this breakpoint as needed
+                settings: {
+                    slidesToShow: 3,
+                },
+            },
+        ],
+    });
+
+    // Change the dot color to orange for both active and inactive dots
+    $('.slider .slick-dots li button').css('color', 'orange');
+});
+
+window.addEventListener('load', function () {
+    const welcomeAnimation = document.querySelector('.welcome-animation');
+    welcomeAnimation.style.animation = 'none'; // Reset animation
+    welcomeAnimation.offsetHeight; // Trigger a reflow
+    welcomeAnimation.style.animation = null; // Re-enable animation
+
+    // Add an event listener to remove the element when the animation ends
+    welcomeAnimation.addEventListener('animationend', function () {
+        welcomeAnimation.remove();
+    });
+});
+
+function scrollToTop() {
+    window.scrollTo({ top: 0, behavior: 'smooth' }); // Smooth scroll to the top
+}
+
+window.addEventListener('load', scrollToTop);
+
+  
