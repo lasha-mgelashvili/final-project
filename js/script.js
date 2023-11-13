411b6c8f350f7eac9a4600950675798b5052a769
Mgela19 <lashamgelashvili777@gmail.com>
2023-11-13 04:18:07 +0400

    smalll-changes

--- js/script.js
+++ js/script.js
@@ -1,61 +1,3 @@
-// let navigationelement = document.getElementById ('navblock');
-// let burgerbar =document.getElementById ('burgerblock');
-
-// burgerbar.addEventListener ('click', function(){
-//     navigationelement.classList.toggle ("newnavigation");
-// });
-
-// $(document).ready(function(){
-//     $('.slider').slick({
-//         slidesToShow: 2,  // Change the number of slides shown
-//         autoplay: false, // Enable autoplay
-//         autoplaySpeed: 3000, // Autoplay speed in milliseconds (adjust as needed)
-//         arrows: true, // Show navigation arrows
-//         dots: true, // Show pagination dots
-//         infinite: false  // Set infinite to false
-//     });
-//   });
-  
-//   $(document).ready(function(){
-//       $('.slider').slick({
-//           slidesToShow: 2,
-//           autoplay: false,
-//           autoplaySpeed: 3000,
-//           arrows: true,
-//           dots: true,
-
-//       });
-  
-//       // Change the dot color to orange for both active and inactive dots
-//       $('.slider .slick-dots li button').css('color', 'orange');
-//   });
-  
-  
-
-
-  
-  
-  
-  
-//   // script.js
-//   window.addEventListener('load', function() {
-//     const welcomeAnimation = document.querySelector('.welcome-animation');
-//     welcomeAnimation.style.animation = 'none'; // Reset animation
-//     welcomeAnimation.offsetHeight; // Trigger a reflow
-//     welcomeAnimation.style.animation = null; // Re-enable animation
-  
-//     // Add an event listener to remove the element when the animation ends
-//     welcomeAnimation.addEventListener('animationend', function() {
-//       welcomeAnimation.remove();
-//     });
-//   });
-//   function scrollToTop() {
-//     window.scrollTo({ top: 0, behavior: 'smooth' }); // Smooth scroll to the top
-//   }
-//   window.addEventListener('load', scrollToTop);
-  
-
-
 
 let navigationelement = document.getElementById('navblock');
 let burgerbar = document.getElementById('burgerblock');
@@ -72,11 +14,13 @@ $(document).ready(function () {
         autoplaySpeed: 3000,
         arrows: true,
         dots: true,
+        Infinity: false,
         responsive: [
             {
                 breakpoint: 768, // Adjust this breakpoint as needed
                 settings: {
                     slidesToShow: 1,
+                    Infinity: true
                 },
             },
             {
